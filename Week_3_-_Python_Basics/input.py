e94d726ab10cd4206ee4934a949fd79d96fd6a00
Ekojoka Chris Idakwo <chris.idakwo@gmail.com>
2025-12-10 23:18:31 +0100

    Updates from the last class session in Week 3

---
+++ Week_3_-_Python_Basics/input.py
@@ -0,0 +1,34 @@
+# first_name = input("What is your first name? ")
+# last_name = input("What is your last name? ")
+# score = input("What was your score? ")
+
+# message = f"{first_name} {last_name} scored {score} out of 100"
+# print(message)
+
+# print("")
+# print("=========================================================")
+# print("Modifying user input")
+# print("=========================================================")
+# print("")
+
+# Method/function chaining
+# Call a function/method
+
+# email_address = input("Enter your email address: ")
+# password = input("Enter your password: ")
+
+# print(email_address.strip().replace("email", "outlook")) #whitespace
+# print(password)
+
+# print("")
+# print("=========================================================")
+# print("=========================================================")
+# print("=========================================================")
+# print("")
+
+# Here, we attempt to convert a user input from string to float so that we can
+# perform a math operation on the providd score value.
+score = input("What was your score? ")
+print((float(score) / 100) * 10)
+
+
