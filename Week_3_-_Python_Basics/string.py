953c800dc4fc16af1443b670b5af5417757058c9
Ekojoka Chris Idakwo <chris.idakwo@gmail.com>
2025-12-10 23:18:31 +0100

    Updates from the last class session in Week 3

--- Week_3_-_Python_Basics/string.py
+++ Week_3_-_Python_Basics/string.py
@@ -58,6 +58,9 @@ print("")
 
 # Conversions
 print("******** Conversions ********")
+# Int => Float
+# Int => Float
+# Int => Float
 
 age = 45
 print(type(age))
@@ -79,11 +82,71 @@ print('===========================')
 # String slicing
 sentence = "Chris Idakwo is in the class session which starts at 9am"
 students = "David,Daniel,Sarah,Lola,Iniye,Irine"
+# TODO: Talk about string split when looking at lists
 
 # Index position by computing
 # -1 less
 greeting = "Hello, good morning"
-print(greeting[0:5])
+print(greeting[:])
+# ---> Format: string[start:end:step]
 
-# print(len(greeting))
+# Reversing a string using a negative step
+print(greeting[::-1])
+
+
+
+print("")
+print("---------------------------------------------")
+print("")
+
+# Strip
+print("******** Strip ********")
+
+username = "  chris.idakwo@email.com  "
+print("Username", username)
+
+# lstrip() removes leading and trailing characters (on the both sides of the string)
+print("Username", username.strip("  "))
+print("-/user@gmail.com&-".strip("-&/"))
+
+# lstrip() removes leading characters (on the left-hand side of the string)
+print("Username", username.lstrip("  "))
+
+#  rstrip() removes trailing characters (on the right-hand side of the string)
+print("Username", username.rstrip("  "))
+
+
+print("")
+print("---------------------------------------------")
+print("")
+
+# String Concatenation
+print("******** String Concatenation ********")
+
+first_name = "John"
+last_name = "Doe"
+score = 80
+
+full_name = first_name + " " + last_name
+print("Full Name:", full_name)
+
+# Print out: John Doe scored 80 out of 100
+sentence = first_name + " " + last_name + " scored " + str(score) + " out of 100"
+print(sentence)
+
+
+# print("")
+print("---------------------------------------------")
+print("")
+
+# String Formatting
+print("******** String Formatting ********")
+# F-String
+sentence_f = f"{first_name} {last_name} scored {score} out of 100"
+print(sentence_f)
+
+sentence_fa = "{lname} {fname} scored {sc} out of 100 for this session!".format(
+    fname=first_name, lname=last_name, sc=score
+)
+print(sentence_fa)
 
