52f8a91621235147d1eb9488827046d170580573
Ekojoka Chris Idakwo <chris.idakwo@gmail.com>
2026-01-07 21:50:30 +0100

    Updates from Day 3, Week 5

---
+++ Week_5_-_Dictionaries/intro.py
@@ -0,0 +1,31 @@
+# A dictionary is a data structure used to store data values in key-value pairs.
+# Values in a dictionary can be of any data type
+# Keys must be strings
+# A dictionary is an ordered collection (as from version 3.17) which is changeable/mutable
+# keys must be unique. Duplicate keys will overwrite the previous value.
+from pprint import pprint
+
+student_a = {
+    "first_name": "Chris",
+    "first_name": "Chris",
+    "last_name": "Idakwo",
+    "age": 20,
+    "height": 1.65,
+    "gender": "Male",
+    "registered": True,
+    "skills": [],
+}
+
+# Create dictionary using dict() constructor
+student_b = dict(
+    first_name = "Chris",
+    last_name = "Idakwo",
+    age = 20,
+    height = 20,
+    gender = "Male",
+)
+
+print("="*50)
+pprint(student_a)
+print("="*50)
+pprint(student_b)
