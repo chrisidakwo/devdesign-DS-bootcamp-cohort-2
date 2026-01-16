12abcf2f267abde8e38baae6fb159a61232c1524
Ekojoka Chris Idakwo <chris.idakwo@gmail.com>
2026-01-16 19:19:18 +0100

    Removed comments from some lines

--- Week_5_-Working_with_Files/intro.py
+++ Week_5_-Working_with_Files/intro.py
@@ -1,11 +1,11 @@
 
 
 # Step 1: Opening and Reading a File
-# file = open("Sample - Superstore.csv", "r")
-# content = file.read()
-# file.close()
+file = open("Sample - Superstore.csv", "r")
+content = file.read()
+file.close()
 
-# print(content)
+print(content)
 
 # Absolute file path: Path to the file on your laptop/deskop or cloud/online
 # c:/Users/chris/Documents/Personal/Bootcamp/Python/intro.py
@@ -14,18 +14,18 @@
 # Relative file path: Path to the file relative to the Python file importing it.
 
 # Step 2: Reading Line by Line (Row by Row)
-# file_ = open("Sample - Superstore.csv", "r")
+file_ = open("Sample - Superstore.csv", "r")
 
-# # Read first line (header)
-# header = file_.readline()
-# header.split(",")
+# Read first line (header)
+header = file_.readline()
+header.split(",")
 
-# # Read all other lines
-# for line in file_:
-#     print(line.split(","))
-#     print("="*20)
+# Read all other lines
+for line in file_:
+    print(line.split(","))
+    print("="*20)
 
-# file_.close()
+file_.close()
 
 
 # Step 3: Using 'with' statement (Best practice)
@@ -37,5 +37,7 @@ with open("Sample - Superstore.csv", "r") as file:
     for line in file:
         print(line.strip())
         print("="*20)
+
+
 
 
